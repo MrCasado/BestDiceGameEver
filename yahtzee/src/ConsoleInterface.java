df9cf513ba0b723db759b0a35d9d24d8c846ae12
student <student@smt67.(none)>
2014-12-05 14:02:54 +0000

    test

---
+++ yahtzee/src/ConsoleInterface.java
@@ -0,0 +1,15 @@
+
+public interface ConsoleInterface {
+
+	public void writeLineToConSoleWithPrompt(String output);
+	
+	public void writeLineToConSoleWithNewLine(String output);
+	
+	public String readLineFromConsole();
+	
+	public String getPrompt();
+
+	public void setPrompt(String prompt);
+
+	
+}
